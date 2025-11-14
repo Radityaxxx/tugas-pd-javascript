1003b6145ac7333b33d8f8d06a2f452b887a7592
Radityaxxx <radityamuhammadramdhan@gmail.com>
2025-11-14 08:40:59 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let buah = ["Lengkeng", "Mangga", "Jeruk", "Pisyang", "Markisa", "Jambu", "Melon", "Semangka" ]
+
+for (let i = 0; i < buah.length; i++) {
+    if (i % 2 !== 0) {
+        document.write(i+1 + ". " + buah[i] + "<br>")
+    }
+}
+
+// dibuat oleh Gilang w, Raditya M, Naufal
